3bbbdfee921d419eccf56453414b5204ef59a394
giveonaldo <giveonaldo@gmail.com>
2024-09-23 10:18:45 +0700

    create ContextApi for theme

---
+++ src/common/ThemeContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({children}) => {
+    const [theme, setTheme] = useState(
+        () => localStorage.getItem('theme') || 'light'
+    );
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+}

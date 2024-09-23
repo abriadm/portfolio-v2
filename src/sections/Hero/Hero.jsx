fb44e986b5a7313dedced7befdeec8a40854385b
giveonaldo <giveonaldo@gmail.com>
2024-09-23 10:06:40 +0700

    Done with Hero Sections

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/hero-img.png";
 import themeIcon from "../../assets/sun.svg";
+import twitterIcon from "../../assets/twitter-light.svg";
+import githubIcon from "../../assets/github-light.svg";
+import linkedinIcon from "../../assets/linkedin-light.svg";
+import cv from "../../assets/cv.pdf";
 
 function Hero() {
   return (
@@ -11,11 +15,30 @@ function Hero() {
         <img className={styles.colorMode} src={themeIcon} alt="icon theme" />
       </div>
       <div className={styles.info}>
-        <h1>Abriansyah<br/>Adam</h1>
+        <h1>
+          Abriansyah
+          <br />
+          Adam
+        </h1>
         <h2>Web Developer</h2>
         <span>
-            <a href="#"></a>
+          <a href="#" target="_blank">
+            <img src={twitterIcon} alt="Twitter Icon" />
+          </a>
+          <a href="#" target="_blank">
+            <img src={githubIcon} alt="Github Icon" />
+          </a>
+          <a href="#" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin Icon" />
+          </a>
         </span>
+        <p className={styles.description}>
+          Exploring new technology stacks to build dynamic and innovative
+          websites.
+        </p>
+        <a href={cv} download>
+          <button className="hover">Resume</button>
+        </a>
       </div>
     </section>
   );

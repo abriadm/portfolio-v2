24b84e31ee6f6147851b2df03a6838e26de66b07
giveonaldo <giveonaldo@gmail.com>
2024-09-24 13:41:45 +0700

    import Projects components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Hero from './sections/Hero/Hero'
+import Projects from './sections/Projects/Projects'
 
 function App() {
 
   return (
     <>
       <Hero />
+      <Projects />
     </>
   )
 }

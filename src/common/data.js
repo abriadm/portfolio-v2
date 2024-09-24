b7a987ddc87e575ed98192c28fa211c97c601ac2
giveonaldo <giveonaldo@gmail.com>
2024-09-24 14:13:36 +0700

    store data for projects

---
+++ src/common/data.js
@@ -0,0 +1,27 @@
+import hipsster from '../assets/hipsster.png';
+import viberr from '../assets/viberr.png';
+import freshBurger from '../assets/fresh-burger.png';
+
+export const projects = [
+    {
+        id: 0,
+        src: hipsster,
+        link: "https://github.com/Ade-mir/company-landing-page-2",
+        h3: "Hipsster",
+        p: "Glasses Shop"
+    },
+    {
+        id: 1,
+        src: viberr,
+        link: "https://github.com/Ade-mir/company-landing-page-2",
+        h3: "Viberr",
+        p: "Streaming App"
+    },
+    {
+        id: 2,
+        src: freshBurger,
+        link: "https://github.com/Ade-mir/company-landing-page-2",
+        h3: "Fresh Burger",
+        p: "Hamburger Restaurant"
+    },
+];

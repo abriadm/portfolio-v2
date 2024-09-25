7c3910be18e6f77a9c67a61da64f8117a485f0b2
giveonaldo <giveonaldo@gmail.com>
2024-09-25 14:20:01 +0700

    add contact section component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Contact from './sections/Contact/Contact'
 import Hero from './sections/Hero/Hero'
 import Projects from './sections/Projects/Projects'
 import Skills from './sections/Skills/Skills'
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Projects />
       <Skills />
+      <Contact />
     </>
   )
 }
